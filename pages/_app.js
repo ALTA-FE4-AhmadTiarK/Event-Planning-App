edb7525adf4c8385a821f6cd586e99fd6b5f8a96
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-12 13:36:36 +0700

    import bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Script from 'next/script';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
