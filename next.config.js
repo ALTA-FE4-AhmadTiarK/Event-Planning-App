e87e43a0e86c33e81679ffe194ea416f7ec7947b
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-17 20:09:47 +0700

    whitelist image from aws

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ['altagroup4.s3.ap-southeast-1.amazonaws.com'],
+	},
+};
 
 module.exports = nextConfig
