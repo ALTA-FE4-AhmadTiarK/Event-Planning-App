29e089ee1c7fd153189529cba7338b138bf3f487
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-17 14:15:24 +0700

    Homepage:
    - perbaikan image (sekarang sudah muncul)
    - perbaikan penataan (ku nonaktifkan banyak dari module.css) kuganti ke bootstrap style
    - pembagian jarak gambar & detail = col-4 & col-8
    - pengaturan model date = menggunakan minute js

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,32 +2,51 @@ import React from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-
+import moment from 'moment';
 
 export default function Card(props) {
-  return (
-    <>
-      <div className={styles.cardComponent} onClick={props.onClick}>
-        <div className={styles.rowComponent}>
-          <div className={styles.imageContainer}>
-            <div className={styles.image}>
-            <Image
-                  src={"/BigThumbnail.svg"}
-                  alt="Picture of the author"
-                  width={300}
-                  height={200}
-                />
-            </div>
-          </div>
-          <div className={styles.detailsContainer}>
-            <div className={styles.contentComponent}>
-              <p>{props.date}</p>
-               <h3>{props.name}</h3>
-              <p className="text-muted ms-1 mb-1">Hosted by {props.host} - {props.location}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.cardComponent} onClick={props.onClick}>
+				{/* <div className={styles.rowComponent}> */}
+				{/* <div className={styles.imageContainer}> */}
+				{/* <div className={styles.image}> */}
+				<div className='row justify-content-center'>
+					<div className='col-lg-4 mx-auto'>
+						<Image
+							// src={'/BigThumbnail.svg'}
+							src={
+								props.image ? props.image : '/BigThumbnail.svg'
+							}
+							className='border border-1'
+							style={{ borderRadius: 1 + 'em' }}
+							alt='Picture of the author'
+							width={300}
+							height={200}
+						/>
+					</div>
+					{/* </div> */}
+					{/* </div> */}
+					{/* <div className={styles.detailsContainer}>
+            <div className={styles.contentComponent}> */}
+					<div className='col-lg-8 my-auto'>
+						{/* <p>{props.date}</p> */}
+						{/* <p>{moment(props.date).format('dddd')},</p> */}
+						<h5 className=''>
+							{moment(props.date).format('dddd')},{' '}
+							{moment(props.date).format('MMMM Do')} @{' '}
+							{moment(props.date).format('LT')} WIB
+						</h5>
+						<h3>{props.name}</h3>
+						<p className='text-muted'>
+							Hosted by {props.host} - {props.location}
+						</p>
+					</div>
+				</div>
+				{/* </div> */}
+				{/* </div> */}
+				{/* </div> */}
+			</div>
+		</>
+	);
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -21,111 +21,108 @@ export default function Home() {
   }, []);
 
   const fetchEvent = async () => {
-    await axios
-      .get(`https://haudhi.site/event`)
-      .then((response) => {
-        setDataEvent(response.data.data);
-        setDisplayData(response.data.data.slice(0, 4));
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => setIsReady(true));
+		await axios
+			.get(`https://haudhi.site/event`)
+			.then((response) => {
+				setDataEvent(response.data.data);
+				setDisplayData(response.data.data.slice(0, 4));
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+			.finally(() => setIsReady(true));
   };
-  
+
   const escapeRegExp = (value) => {
-    return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+		return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
   };
-  
+
   const requestSearch = (searchValue) => {
-    const searchRegex = new RegExp(escapeRegExp(searchValue), "i");
-    const filteredData = dataEvent.filter((row) => {
-      return Object.keys(row).some((field) => {
-        return searchRegex.test(row[field] ? row[field] : null);
-      });
-    });
-    setDisplayData(filteredData);
-    console.log(filteredData);
-  }
+		const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
+		const filteredData = dataEvent.filter((row) => {
+			return Object.keys(row).some((field) => {
+				return searchRegex.test(row[field] ? row[field] : null);
+			});
+		});
+		setDisplayData(filteredData);
+		console.log(filteredData);
+  };
 
   let result;
   if (isReady) {
-    let sliced = dataEvent.slice(0, sliceData);
-    result = (
-      <>
-        {dispalyData.map((item, index) => {
-          return (
-            <Card
-              key={index}
-              id={item.ID}
-              image={item.image}
-              name={item.name}
-              host={item.host}
-              date={item.date}
-              location={item.location}
-              onClick={() => {
-                router.push(`/event/${item.ID}`);
-              }}
-            />
-          );
-        })}
-        <div className={styles.viewMore}>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            onClick={async () => {
-              await setSliceData(sliceData + 4);
-              setDisplayData(dataEvent.slice(1, sliceData));
-            }}
-          >
-            View More
-          </button>
-        </div>
-      </>
-    );
+		let sliced = dataEvent.slice(0, sliceData);
+		result = (
+			<>
+				{dispalyData.map((item, index) => {
+					return (
+						<Card
+							key={index}
+							id={item.id}
+							image={item.image}
+							name={item.name}
+							host={item.host}
+							date={item.date}
+							location={item.location}
+							onClick={() => {
+								router.push(`/event/${item.id}`);
+							}}
+						/>
+					);
+				})}
+				<div className={styles.viewMore}>
+					<button
+						type='button'
+						className='btn btn-outline-success'
+						onClick={async () => {
+							await setSliceData(sliceData + 4);
+							setDisplayData(dataEvent.slice(1, sliceData));
+						}}>
+						View More
+					</button>
+				</div>
+			</>
+		);
   } else {
-    result = "no data";
+		result = 'no data';
   }
 
   return (
-    <>
-      <Head>
-        <title>Home Page</title>
-        <meta name="login" content="Home" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+		<>
+			<Head>
+				<title>Home Page</title>
+				<meta name='login' content='Home' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-      <Layout
-        onChange={(e) => requestSearch(e.target.value)}
-      >
-        <main>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 mx-auto">
-                {/* <div className={styles.box}></div> */}
-                <div className={styles.category}>
-                  <ul>
-                    <li href="">GAMES</li>
-                    <li href="">MOVIE</li>
-                    <li href="">SPORT</li>
-                    <li href="">ART</li>
-                    <li href="">FOOD</li>
-                    <li href="">EDUCATION</li>
-                    <li href="">PARTY</li>
-                    <li href="">MUSIC</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="row border-bottom border-3 border-dark mt-5 mb-3">
-              <div className="col-lg-12">
+			<Layout onChange={(e) => requestSearch(e.target.value)}>
+				<main>
+					<div className='container'>
+						<div className='row'>
+							<div className='col-lg-12 mx-auto'>
+								{/* <div className={styles.box}></div> */}
+								<div className={styles.category}>
+									<ul>
+										<li href=''>GAMES</li>
+										<li href=''>MOVIE</li>
+										<li href=''>SPORT</li>
+										<li href=''>ART</li>
+										<li href=''>FOOD</li>
+										<li href=''>EDUCATION</li>
+										<li href=''>PARTY</li>
+										<li href=''>MUSIC</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div className='row border-bottom border-3 border-dark mt-5 mb-3'>
+							{/* <div className="col-lg-12">
                 <p className="text-muted ms-1 mb-1">Today</p>
-              </div>
-            </div>
-            {result}
-          </div>
-        </main>
-      </Layout>
-    </>
+              </div> */}
+						</div>
+						{result}
+					</div>
+				</main>
+			</Layout>
+		</>
   );
 }
