c23bbbedac8c027dcfa642bf0a70253e0eb9edfd
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-18 05:46:54 +0700

    cleaning up unused variables

--- pages/myevent/[userID].jsx
+++ pages/myevent/[userID].jsx
@@ -1,76 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import Head from "next/head";
 import Layout from '../../components/Layout';
-import Image from "next/image";
 import axios from 'axios';
-import { fetchUser } from '../../func/fetch';
 import { useRouter } from 'next/router';
 import CardMyEvent from '../../components/CardMyEvent';
 
 export default function MyEvent() {
-  //const [username, setUsername] = useState('');
-  const [displayedData, setDisplayedData] = useState([]);
-  const router = useRouter();
+	const [displayedData, setDisplayedData] = useState([]);
+	const router = useRouter();
 
-  useEffect(() => {
-	    fetchEvent();
-		
+	useEffect(() => {
+		fetchEvent();
 	}, []);
-  
 
-  const fetchEvent = async () => {
-    //const { userID } = router.query;
-    await axios
-      .get(`https://haudhi.site/event/user/`,{
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('token'),
-        },
-      })
-      .then((response) => {
-        console.log(response.data.data);
-        setDisplayedData(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  };
-  
-  return (
-    <>
-      <Head>
-        <title>My Event</title>
-        <meta name="login" content="My Event" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const fetchEvent = async () => {
+		await axios
+			.get(`https://haudhi.site/event/user/`, {
+				headers: {
+					Authorization: 'Bearer ' + localStorage.getItem('token'),
+				},
+			})
+			.then((response) => {
+				setDisplayedData(response.data.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-      <Layout>
-        <main>
-          <div className="container">
-            <div className="row border-bottom border-3 border-dark mt-5">
-              <div className="col-lg-12">
-                <h2>My Event</h2>
-                </div>
-              </div>
-              {displayedData.map((item, index) => {
-					return (
-						<CardMyEvent
-							key={index}
-							id={item.id}
-							image={item.image}
-							name={item.name}
-							host={item.host}
-							date={item.date}
-							location={item.location}
-                            onClick={() => {
-								router.push(`/event/${item.id}`);
-							}}
-							
-						/>
-					);
-				})}
-          </div>
-        </main>
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>My Event</title>
+				<meta name='login' content='My Event' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Layout>
+				<main>
+					<div className='container'>
+						<div className='row border-bottom border-3 border-dark mt-5'>
+							<div className='col-lg-12'>
+								<h2>My Event</h2>
+							</div>
+						</div>
+						{displayedData.map((item, index) => {
+							return (
+								<CardMyEvent
+									key={index}
+									id={item.id}
+									image={item.image}
+									name={item.name}
+									host={item.host}
+									date={item.date}
+									location={item.location}
+									onClick={() => {
+										router.push(`/event/${item.id}`);
+									}}
+								/>
+							);
+						})}
+					</div>
+				</main>
+			</Layout>
+		</>
+	);
 }
