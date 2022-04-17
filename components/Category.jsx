a84425239e1ff0590504ecdfc003758a406f59a6
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-18 05:38:03 +0700

    fixing filter category

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,35 +1,69 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from 'react';
 
 function Category({
-  activeCategory,
-  setActiveCategory,
-  displayData,
-  setCategory,
+	activeCategory,
+	setActiveCategory,
+	displayData,
+	setCategory,
 }) {
-  useEffect(() => {
-    if (activeCategory === 0) {
-      setCategory(displayData);
-      return;
-    }
-    const filtered = displayData.filter((item) =>
-      item.category_id_includes(activeCategory)
-    );
-    setCategory(filtered);
-  }, [activeCategory, displayData, setCategory]);
+	useEffect(() => {
+		if (activeCategory === 0) {
+			setCategory(displayData);
+			return;
+		}
+		const filtered = Object.values(displayData).filter(
+			(item) => item.category_id === activeCategory
+		);
+		setCategory(filtered);
+	}, [activeCategory, displayData, setCategory]);
 
-  return (
-    <>
-    <div className="d-flex flex-wrap justify-content-evenly">
-    <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(0)}>All</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(1)}>Games</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(2)}>Art</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(3)}>Sport</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(4)}>Technology</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(5)}>Music</button>
-      <button type="button" className="btn btn-outline-success" onClick={() => setActiveCategory(6)}>Education</button>
-    </div>
-      
-    </>
-  );
+	return (
+		<>
+			<div className='row justify-content-evenly'>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(0)}>
+					All
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(1)}>
+					Games
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(2)}>
+					Art
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(3)}>
+					Sport
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(4)}>
+					Technology
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(5)}>
+					Music
+				</button>
+				<button
+					type='button'
+					className='btn btn-outline-success col m-2'
+					onClick={() => setActiveCategory(6)}>
+					Education
+				</button>
+			</div>
+		</>
+	);
 }
 export default Category;
