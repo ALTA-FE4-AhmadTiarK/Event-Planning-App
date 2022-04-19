67c1bdbb230091b0d3634e0bc556df5e1e38feb3
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-19 14:01:46 +0700

    refactoring alert

---
+++ func/alert.js
@@ -0,0 +1,81 @@
+import Swal from 'sweetalert2';
+
+function loginAlert() {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: 'You must be logged in to make a comment',
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function fillAll() {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: 'Please fill all the fields',
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function emptyComment() {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: 'Comment cannot be empty',
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function successMessage(res) {
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: res.data.message,
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function errorMessage(err) {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: err.response.data.message,
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function quotaFull() {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: 'Quota is full',
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+function notHost() {
+	Swal.fire({
+		position: 'center',
+		icon: 'error',
+		title: 'You are not the host of this event',
+		showConfirmButton: false,
+		timer: 1500,
+	});
+}
+
+export {
+	emptyComment,
+	successMessage,
+	errorMessage,
+	loginAlert,
+	quotaFull,
+	fillAll,
+	notHost,
+};
